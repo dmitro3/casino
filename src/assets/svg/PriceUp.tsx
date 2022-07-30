e98951335c4b22d88e0777d1d59a706732608ce9
Konstantin300 <topaykink@mail.ru>
2022-07-30 16:26:12 +0600

    add currency cards

---
+++ src/assets/svg/PriceUp.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const PriceUp = () => {
+  return (
+    <svg
+      width="11"
+      height="14"
+      viewBox="0 0 11 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 6L5.5 1.5L10 6M1 12.5L5.5 8L10 12.5"
+        stroke="#4ED254"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default PriceUp;
---
+++ src/pages/home/components/CurrencyCard/CurrencyCard.tsx
@@ -0,0 +1,49 @@
+import React, { FC } from 'react';
+import Image from 'next/image';
+
+import { PriceUpIcon } from 'src/assets/svg';
+import styles from './CurrencyCard.module.scss';
+
+type Props = {
+  key: number;
+  imageUrl: any;
+  name: string;
+  priceChange: string;
+  currentPrice: string;
+  volume: string;
+};
+
+const CurrencyCard: FC<Props> = ({
+  id,
+  imageUrl,
+  name,
+  priceChange,
+  currentPrice,
+  volume,
+}) => {
+  return (
+    <div className={styles.currencyCard}>
+      <div className={styles.currencyCardHeader}>
+        <Image
+          src={imageUrl}
+          height={46}
+          width={46}
+          layout="fixed"
+          className={styles.currencyCardImage}
+        />
+        <span className={styles.currencyCardName}>{name}</span>
+        <span className={styles.currencyCardPriceChange}>{priceChange}</span>
+      </div>
+      <div className={styles.currencyCardContent}>
+        <span className={styles.currencyCardPriceCurrent}>
+          <PriceUpIcon /> {currentPrice}
+        </span>
+        <span
+          className={styles.currencyCardVol}
+        >{`24h Volume: ${volume}`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyCard;
