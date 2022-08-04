3644ec9d9e7c32e8d5cd4294614d76af1b513f97
Konstantin300 <topaykink@mail.ru>
2022-08-04 11:02:32 +0600

    fix acc navigation

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 import Navigation from 'src/pages/account/components/Navigation';
-import ProfileInfo from 'src/pages/account/components/ProfileInfo';
 import MainLayout from 'src/components/MainLayout';
 import styles from 'src/pages/account/index.module.scss';
 
 const Account: NextPage = () => {
+  const router = useRouter();
+
   return (
     <MainLayout hasMaxWidth>
       <section className={styles.container}>
-        <span className={styles.backButton}>{`< Back`}</span>
+        <button
+          type="button"
+          className={styles.backButton}
+          onClick={() => router.back()}
+        >{`< Back`}</button>
         <p className={styles.mainLabel}>Account</p>
         <Navigation />
-        <ProfileInfo />
       </section>
     </MainLayout>
   );
