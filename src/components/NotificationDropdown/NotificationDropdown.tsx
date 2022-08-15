a4ab8eee0e748db721058342c96262cee5dbaed8
Konstantin300 <topaykink@mail.ru>
2022-08-15 18:04:10 +0600

    add notification header

--- src/components/NotificationDropdown/NotificationDropdown.tsx
+++ src/components/NotificationDropdown/NotificationDropdown.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Cards from 'src/components/NotificationDropdown/Cards';
+import NotificationHeader from 'src/components/NotificationDropdown/NotificationHeader';
 import styles from './NotificationDropdown.module.scss';
 
 const NotificationDropdown = () => {
   return (
     <aside className={styles.container}>
+      <NotificationHeader />
       <Cards />
     </aside>
   );
