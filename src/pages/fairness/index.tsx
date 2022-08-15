6c58d05bf305f15bd4d213fa96ad47cc24c809b6
Konstantin300 <topaykink@mail.ru>
2022-08-15 21:12:11 +0600

    add routing for cashback, rank, daily, fairness

---
+++ src/pages/fairness/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import MainLayout from 'src/components/MainLayout';
+import BackButton from 'src/components/BackButton';
+
+const index = () => {
+  return (
+    <MainLayout>
+      <BackButton />
+    </MainLayout>
+  );
+};
+
+export default index;
