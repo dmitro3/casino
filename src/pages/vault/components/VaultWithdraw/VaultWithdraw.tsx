b8470922ccbff516716ccc170152cb24febb1300
Konstantin300 <topaykink@mail.ru>
2022-08-18 18:06:06 +0600

    add sidebar to withdraw page

--- src/pages/vault/components/VaultWithdraw/VaultWithdraw.tsx
+++ src/pages/vault/components/VaultWithdraw/VaultWithdraw.tsx
@@ -4,6 +4,7 @@ import Dropdown from 'src/components/Dropdown';
 import WalletDropdownButton from 'src/components/Header/components/WalletDropdownButton';
 import WalletDropdownWindow from 'src/components/Header/components/WalletDropdownWindow';
 import NetworkDropdown from 'src/pages/vault/components/VaultWithdraw/components/NetworkDropdown';
+import VaultSidebar from 'src/pages/vault/components/VaultSidebar';
 import Input from 'src/components/Input';
 import Button from 'src/components/Button';
 import { TetherToken, CloseIcon, AttentionIcon } from 'src/assets/svg';
@@ -12,19 +13,21 @@ import styles from './VaultWithdraw.module.scss';
 export const DepositDropdown = () => {
   return (
     <div className={styles.root}>
-      <div className={styles.dropdown}>
-        <Dropdown
-          buttonComponent={<WalletDropdownButton />}
-          dropdownComponent={<WalletDropdownWindow />}
-          customButtonStyles={styles.walletDropdown}
-          customWalletDropdownStyles={styles.windowDropdown}
-        />
-        <Dropdown
-          buttonComponent={<NetworkDropdown />}
-          dropdownComponent={<div />}
-          customButtonStyles={styles.walletDropdown}
-          customWalletDropdownStyles={styles.windowDropdown}
-        />
+      <div>
+        <div className={styles.dropdown}>
+          <Dropdown
+            buttonComponent={<WalletDropdownButton />}
+            dropdownComponent={<WalletDropdownWindow />}
+            customButtonStyles={styles.walletDropdown}
+            customWalletDropdownStyles={styles.windowDropdown}
+          />
+          <Dropdown
+            buttonComponent={<NetworkDropdown />}
+            dropdownComponent={<div />}
+            customButtonStyles={styles.walletDropdown}
+            customWalletDropdownStyles={styles.windowDropdown}
+          />
+        </div>
       </div>
     </div>
   );
@@ -32,41 +35,46 @@ export const DepositDropdown = () => {
 
 const VaultWithdraw = () => {
   return (
-    <section className={styles.root}>
-      <span>Withdraw</span>
-      <DepositDropdown />
-      <div className={styles.inputContainerWide}>
-        <span>Адрес вашего кошелька</span>
-        <Input placeholder="Enter address" customStyles={styles.input} />
-      </div>
-      <div className={styles.inputContainerWide}>
-        <span>Сумма</span>
-        <div className={styles.sumContainer}>
-          <Input
-            placeholder="Enter Amount"
-            customStyles={`${styles.inputContainer} ${styles.inputAmount}`}
-            additionalButton={
-              <button className={styles.buttonAdditional}>all</button>
-            }
-          />
-          <Input
-            value="0.00000000"
-            placeholder=""
-            icon={<TetherToken height={20} />}
-            disabled
-            customStyles={`${styles.inputContainer}`}
-            additionalButton={<CloseIcon width={14} height={14} />}
-          />
+    <section className={`${styles.root} ${styles.sidebarContainer}`}>
+      <aside>
+        <VaultSidebar />
+      </aside>
+      <div>
+        <span>Withdraw</span>
+        <DepositDropdown />
+        <div className={styles.inputContainerWide}>
+          <span>Адрес вашего кошелька</span>
+          <Input placeholder="Enter address" customStyles={styles.input} />
         </div>
-      </div>
-      <div />
-      <div className={styles.attentionTab}>
-        <AttentionIcon />
-        <span>Минимальная сумма вывода составляет 25 USDT</span>
-      </div>
-      <Button label="Снять" customStyles={styles.button} />
-      <div className={styles.smallText}>
-        <span>Transaction free - 1.5 USDT.</span>
+        <div className={styles.inputContainerWide}>
+          <span>Сумма</span>
+          <div className={styles.sumContainer}>
+            <Input
+              placeholder="Enter Amount"
+              customStyles={`${styles.inputContainer} ${styles.inputAmount}`}
+              additionalButton={
+                <button className={styles.buttonAdditional}>all</button>
+              }
+            />
+            <Input
+              value="0.00000000"
+              placeholder=""
+              icon={<TetherToken height={20} />}
+              disabled
+              customStyles={`${styles.inputContainer}`}
+              additionalButton={<CloseIcon width={14} height={14} />}
+            />
+          </div>
+        </div>
+        <div />
+        <div className={styles.attentionTab}>
+          <AttentionIcon />
+          <span>Минимальная сумма вывода составляет 25 USDT</span>
+        </div>
+        <Button label="Снять" customStyles={styles.button} />
+        <div className={styles.smallText}>
+          <span>Transaction free - 1.5 USDT.</span>
+        </div>
       </div>
     </section>
   );
