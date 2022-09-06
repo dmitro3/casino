359dfad836fced7e7aae997e16ce7ce0a616fb59
Mark-Alyayskiy <malyayskiy@mail.ru>
2022-09-06 20:12:00 +0600

    add sort news on blog page and select category

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import * as auth from './auth';
 import * as article from './article';
 import * as user from './user';
+import * as category from './category';
 
-export { auth, article, user };
+export { auth, article, user, category };
 
 export const BASE_URL = process.env.NEXT_PUBLIC_API_ENDPOINT;
